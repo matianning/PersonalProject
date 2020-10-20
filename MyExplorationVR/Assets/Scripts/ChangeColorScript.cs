5f25f2274a0167263c964bd58f58a3add35d18a8
matianning <60898311+matianning@users.noreply.github.com>
2020-10-20 22:12:56 +0200

    MyExplorationVR - Création

---
+++ MyExplorationVR/Assets/Scripts/ChangeColorScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ChangeColorScript : MonoBehaviour
+{
+    public Material red;
+    public Material blue;
+    public Material magenta;
+
+    public void Red()
+    {
+        Debug.Log("red");
+        GetComponent<MeshRenderer>().material = red;
+    }
+
+    public void Blue()
+    {
+        Debug.Log("blue");
+        GetComponent<MeshRenderer>().material = blue;
+    }
+
+    public void Magenta()
+    {
+        Debug.Log("purple");
+        GetComponent<MeshRenderer>().material = magenta;
+    }
+}
---
+++ MyExplorationVR/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool walking = true;
+    private Vector3 spawnPoint;
+    private Camera mainCamera;
+    public float speed = 1.5f;
+  
+    void Start()
+    {
+        mainCamera = Camera.main;
+        spawnPoint = transform.position;
+    }
+
+    void Update()
+    {
+        if (walking)
+        {
+            transform.position += speed * mainCamera.transform.forward * 0.5f * Time.deltaTime;
+        }
+
+        if (transform.position.y < -10f)
+        {
+            transform.position = spawnPoint;
+        }
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.name.Contains("Plane"))
+            {
+                walking = false;
+            }
+            else
+            {
+                walking = true;
+            }
+        }
+        else
+        {
+            walking = true;     
+        }
+    }
+}
