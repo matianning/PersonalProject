310a80af2f74da9f29fe51c29ea72a85397f1a55
matianning <60898311+matianning@users.noreply.github.com>
2020-10-21 11:28:57 +0200

    MyExplorationVR - Movement du joueur
    Ajout Mouvement du joueur
    Ajout d'interaction avec un canvas

--- MyExplorationVR/Assets/Scripts/PlayerWalkController.cs
+++ MyExplorationVR/Assets/Scripts/PlayerWalkController.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController1 : MonoBehaviour
+public class PlayerWalkController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public Transform vrCamera;
+    public float toggleAngle = 20.0f;
+    public float speed = 5.0f;
+    public bool moveForward;
+
+    private CharacterController cc;
+
+
     void Start()
     {
-        
+        cc = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        //print(vrCamera.eulerAngles.x);
+        if (vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.0f)
+        {
+            moveForward = true;
+        }
+        else
+        {
+            moveForward = false;
+        }
+
+        if (moveForward)
+        {
+            Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+
+            cc.SimpleMove(forward * speed);
+        }
     }
 }
---
+++ MyExplorationVR/Assets/showHideInformation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showHideInformation : MonoBehaviour
+{
+    public GameObject info;
+    private bool show = true;
+
+    public void showInfo()
+    {
+        if (!show)
+        {
+            info.SetActive(true);
+            show = true;
+        }
+        else
+        {
+            info.SetActive(false);
+            show = false;
+        }
+    }
+}
