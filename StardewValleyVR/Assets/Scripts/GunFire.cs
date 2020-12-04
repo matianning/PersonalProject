af7eadf936c329c48fef1982faca18dde80724de
matianning <60898311+matianning@users.noreply.github.com>
2020-12-04 22:58:36 +0100

    StardewValleyVR - settings and bugfix

---
+++ StardewValleyVR/Assets/Scripts/GunFire.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunFire : MonoBehaviour
+{
+    public float speed = 10;
+    public GameObject bullet;
+    public Transform spawnPoint;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+
+    public void Fire()
+    {
+        GameObject spawnedBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
+        spawnedBullet.GetComponent<Rigidbody>().velocity = speed * spawnPoint.forward;
+        if(audioClip!=null)
+            audioSource.PlayOneShot(audioClip);
+        Destroy(spawnedBullet, 2);
+
+    }
+}
